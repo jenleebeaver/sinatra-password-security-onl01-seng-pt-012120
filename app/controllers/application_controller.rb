9cf7c5d78ea38ea05c677967b00a52de1d4a192f
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-04-23 20:28:36 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,39 +9,37 @@ class ApplicationController < Sinatra::Base
 	end
 
 	get "/" do
-		erb :index #signup or login page
+		erb :index
 	end
 
-	get "/signup" do #form to create a new user
+	get "/signup" do
 		erb :signup
 	end
 
 	post "/signup" do
-		  user = User.new(:username => params[:username], :password => params[:password])
-			#creating instance of our user
-			if user.save
-    redirect "/login"
-  else
-    redirect "/failure"
-  end
+		user = User.new(:username => params[:username], :password => params[:password])
+		if user.save
+			redirect "/login"
+		else
+			redirect "/failure"
+		end
 	end
 
-	get "/login" do #form for logging in
+	get "/login" do
 		erb :login
 	end
 
-  post "/login" do
-    user = User.find_by(:username => params[:username])
+	post "/login" do
+		user = User.find_by(:username => params[:username])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect "/success"
+		else
+			redirect "failure"
+		end
+	end
 
-    if user && user.authenticate(params[:password]) #asking us if we have a user and if the user is authenticated
-      session[:user_id] = user.id
-      redirect "/success"
-    else
-      redirect "/failure"
-    end
-  end
-
-	get "/success" do #displayed once user succesfully logs in
+	get "/success" do
 		if logged_in?
 			erb :success
 		else
@@ -49,22 +47,22 @@ class ApplicationController < Sinatra::Base
 		end
 	end
 
-	get "/failure" do #login error
+	get "/failure" do
 		erb :failure
 	end
 
-	get "/logout" do #clears session and redirects to home page or login page
+	get "/logout" do
 		session.clear
 		redirect "/"
 	end
 
 	helpers do
 		def logged_in?
-			!!session[user_id]
+			!!session[:user_id]
 		end
 
 		def current_user
-			User.find(session[user_id])
+			User.find(session[:user_id])
 		end
 	end
 
